4a231282b810ef9b34394d2cb42449230bab4a8f
lc <ziyu0123456789@gmail.com>
2015-06-17 00:39:47 +0800

    测试Restful接口设计

---
+++ src/main/java/com/aiziyuer/webapp/book/Book.java
@@ -0,0 +1,27 @@
+package com.aiziyuer.webapp.book;
+
+import lombok.Data;
+
+/**
+ * 数据类
+ */
+@Data
+public class Book {
+
+    private Integer id;
+
+    private String name;
+
+    private double price;
+
+    public Book(Integer id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Book()
+    {
+
+    }
+}
